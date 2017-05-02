53acfb69e9131573576a4f5c806fe0378e2e0347
Zhihong Ye <yezhihong@gmail.com>
2017-05-01 22:46:24 -0500

    add run.sh script 	new file:   run.sh 	deleted:    solid_neutron_DEMP.aux 	deleted:    solid_neutron_DEMP.bbl 	deleted:    solid_neutron_DEMP.blg 	deleted:    solid_neutron_DEMP.log 	deleted:    solid_neutron_DEMP.out 	modified:   solid_neutron_DEMP.pdf 	deleted:    solid_neutron_DEMP.toc

---
+++ proposals/run.sh
@@ -0,0 +1,11 @@
+pdflatex solid_neutron_DEMP.tex
+pdflatex solid_neutron_DEMP.tex
+bibtex solid_neutron_DEMP
+pdflatex solid_neutron_DEMP.tex
+
+rm -f solid_neutron_DEMP.aux
+rm -f solid_neutron_DEMP.bbl
+rm -f solid_neutron_DEMP.blg
+rm -f solid_neutron_DEMP.log
+rm -f solid_neutron_DEMP.out
+rm -f solid_neutron_DEMP.toc
